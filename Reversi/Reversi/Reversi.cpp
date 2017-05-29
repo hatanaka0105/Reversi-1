fef64831611d41f30db2945beb5733ad1cd6ef92
YukiTakamura <g1527047@st.t-kougei.ac.jp>
2017-05-29 14:52:13 +0900

    不要なコード削除

--- Reversi/Reversi/Reversi.cpp
+++ Reversi/Reversi/Reversi.cpp
@@ -80,62 +80,7 @@ void Reversi::Draw()
 
 	cout << "Drawだよ～" << endl;
 	guide.DrawHorizontal();
-	board.Draw(stone);
-
-	guide.DrawVertical();
-
-	HANDLE hStdout;
-	WORD wAttributes;
-	CONSOLE_SCREEN_BUFFER_INFO csbi;//構造体です
-
-	hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
-	//hStdoutのコンソールスクリーンバッファ情報をcsbiに取得
-	GetConsoleScreenBufferInfo(hStdout, &csbi);
-
-	wAttributes = 7 | FOREGROUND_INTENSITY;
-	SetConsoleTextAttribute(hStdout, wAttributes);
-
-	cout << endl;
-
-	cout << "   "; cout << "　１　２　３　４　５　６　７　８" << endl;
-	wAttributes = wAttributes | BACKGROUND_GREEN;
-	SetConsoleTextAttribute(hStdout, wAttributes);
-
-	cout << "   "; cout << "┏━┳━┳━┳━┳━┳━┳━┳━┓" << endl;
-	cout << " A "; cout << "┃　┃　┃　┃　┃　┃　┃　┃　┃" << endl;
-	cout << "   "; cout << "┣━╋━╋━╋━╋━╋━╋━╋━┫" << endl;
-	cout << " B "; cout << "┃　┃　┃　┃　┃　┃　┃　┃　┃" << endl;
-	cout << "   "; cout << "┣━╋━╋━╋━╋━╋━╋━╋━┫" << endl;
-	cout << " C "; cout << "┃　┃　┃　┃　┃　┃　┃　┃　┃" << endl;
-	cout << "   "; cout << "┣━╋━╋━╋━╋━╋━╋━╋━┫" << endl;
-
-	cout << " D "; cout << "┃　┃　┃　┃●┃";
-	wAttributes = COLOR_BLACK | BACKGROUND_GREEN;
-	SetConsoleTextAttribute(hStdout, wAttributes);
-	cout << "●";
-	wAttributes = COLOR_WHITE | BACKGROUND_GREEN | FOREGROUND_INTENSITY;
-	SetConsoleTextAttribute(hStdout, wAttributes);
-	cout << "┃　┃　┃　┃" << endl;
-	cout << "   "; cout << "┣━╋━╋━╋━╋━╋━╋━╋━┫" << endl;
-	cout << " E "; cout << "┃　┃　┃　┃";
-	wAttributes = COLOR_BLACK | BACKGROUND_GREEN;
-	SetConsoleTextAttribute(hStdout, wAttributes);
-	cout << "●";
-	wAttributes = COLOR_WHITE | BACKGROUND_GREEN | FOREGROUND_INTENSITY;
-	SetConsoleTextAttribute(hStdout, wAttributes);
-	cout << "┃●┃　┃　┃　┃" << endl;
-
-	cout << "   "; cout << "┣━╋━╋━╋━╋━╋━╋━╋━┫" << endl;
-	cout << " F "; cout << "┃　┃　┃　┃　┃　┃　┃　┃　┃" << endl;
-	cout << "   "; cout << "┣━╋━╋━╋━╋━╋━╋━╋━┫" << endl;
-	cout << " G "; cout << "┃　┃　┃　┃　┃　┃　┃　┃　┃" << endl;
-	cout << "   "; cout << "┣━╋━╋━╋━╋━╋━╋━╋━┫" << endl;
-	cout << " H "; cout << "┃　┃　┃　┃　┃　┃　┃　┃　┃" << endl;
-	cout << "   "; cout << "┗━┻━┻━┻━┻━┻━┻━┻━┛" << endl;
-	cout << endl;
-
-	//元のテキスト状態に戻す
-	SetConsoleTextAttribute(hStdout, csbi.wAttributes);
+	board.Draw(stone, guide);
 }
 
 /*
